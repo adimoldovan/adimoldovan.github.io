10b11cf726972b5d364981e37221374a842157b2
Adrian Moldovan <adim.moldovan@gmail.com>
2024-01-31 22:56:00 +0200

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import './style.css';
 import data from './profile.json';
 
-const hash = data.hash;
+const { hash } = data;
 
 function contentComponent() {
-    const content = document.createElement('div');
-    content.id = 'content';
+  const content = document.createElement('div');
+  content.id = 'content';
 
-    const profile = document.createElement('div');
+  const profile = document.createElement('div');
 
-    profile.innerHTML = `<img class="profile-picture" src="https://www.gravatar.com/avatar/${hash}?s=200" alt="profile picture" />`;
-    profile.appendChild(document.createElement('h1')).textContent = data.displayName;
-    profile.appendChild(document.createElement('p')).textContent = data.description;
+  profile.innerHTML = `<img class="profile-picture" src="https://www.gravatar.com/avatar/${hash}?s=200" alt="profile picture" />`;
+  profile.appendChild(document.createElement('h1')).textContent = data.displayName;
+  profile.appendChild(document.createElement('p')).textContent = data.description;
 
-    for (const account of data.accounts) {
-        const link = document.createElement('a');
-        link.href = account.url;
-        link.target = '_blank';
-        link.innerHTML = `<img src="${account.iconUrl}" alt="${account.name}"/>`;
-        profile.appendChild(link);
-    }
+  for (const account of data.accounts) {
+    const link = document.createElement('a');
+    link.href = account.url;
+    link.target = '_blank';
+    link.innerHTML = `<img src="${account.iconUrl}" alt="${account.name}"/>`;
+    profile.appendChild(link);
+  }
 
-    content.appendChild(profile);
-    return content;
+  content.appendChild(profile);
+  return content;
 }
 
 document.body.appendChild(contentComponent());
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    entry: {index: './src/index.js'},
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Adrian Moldovan',
-        }),
+  entry: { index: './src/index.js' },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Adrian Moldovan',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
