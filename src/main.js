ba900234398fcdf95fc7ce7e30c3de76e447051a
Adrian Moldovan <adim.moldovan@gmail.com>
2024-01-31 18:28:52 +0200

    Add webpack basic structure

--- src/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mainContent = document.getElementById('content');
-    mainContent.innerHTML = '<p>This is a simple static site built with vanilla JavaScript, HTML, and CSS.</p>';
-});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {index: './src/index.js'},
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Adrian Moldovan',
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
