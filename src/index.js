ad616f5f67cfbe8d6720e539bcedeb591c186d73
Adrian Moldovan <adim.moldovan@gmail.com>
2024-01-31 22:48:35 +0200

    Add some content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import './style.css';
+import data from './profile.json';
 
-function component() {
-    const element = document.createElement('div');
+const hash = data.hash;
 
-    element.innerHTML = "Hello there!";
-    element.classList.add('hello');
+function contentComponent() {
+    const content = document.createElement('div');
+    content.id = 'content';
 
-    return element;
+    const profile = document.createElement('div');
+
+    profile.innerHTML = `<img class="profile-picture" src="https://www.gravatar.com/avatar/${hash}?s=200" alt="profile picture" />`;
+    profile.appendChild(document.createElement('h1')).textContent = data.displayName;
+    profile.appendChild(document.createElement('p')).textContent = data.description;
+
+    for (const account of data.accounts) {
+        const link = document.createElement('a');
+        link.href = account.url;
+        link.target = '_blank';
+        link.innerHTML = `<img src="${account.iconUrl}" alt="${account.name}"/>`;
+        profile.appendChild(link);
+    }
+
+    content.appendChild(profile);
+    return content;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(contentComponent());
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {index: './src/index.js'},
-    mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Adrian Moldovan',
